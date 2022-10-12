c4c6cb295f7b866b01e3d85bf0ba991e9bd50768
okneylixel <okneylixel@gmail.com>
2022-10-12 20:14:18 +0300

    getpost from db to view on main pg

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import Head from 'next/head'
+import Message from '../components/message'
+import { useEffect, useState } from 'react'
+import { db } from '../utils/firebase'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 
 
 export default function Home() {
+  //Create a state with all the post
+  const [allPosts, setAllPosts] = useState([])
+
+  const getPost = async () => {
+    const collectionRef = collection(db, 'posts')
+    const q = query(collectionRef, orderBy('timestamp', 'desc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setAllPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    })
+    return unsubscribe
+  }
+
+  useEffect(() => {
+    getPost()
+  }, [])
+
   return (
     <div >
       <Head>
@@ -10,7 +30,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main> </main>
+      <div className='my-12 text-lg font-medium'>
+        <h2 >See what other people are saying</h2>
+      </div>
     </div>
   )
 }
